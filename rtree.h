7908e5a98a47793c7f9e09538e206578aefc7237
Sanjeev Mallick <sanjeevmallick2@gmail.com>
2023-04-28 12:35:23 +0530

    implemented insertion partially

--- rtree.h
+++ rtree.h
@@ -1,6 +1,6 @@
 #ifndef RTREE_H
 //------------------------------------------------------------------------------------------------------------------------
-//The dashes like the one shown above show important demarcations and boundaries to make the code readable
+// The dashes like the one shown above show important demarcations and boundaries to make the code readable
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -14,46 +14,52 @@ typedef struct point Point;
 typedef struct rectangle Rect;
 typedef struct node_ele Node_ele;
 
-struct point {
+struct point
+{
     int x;
     int y;
 };
 
 // Assuming the coordinates to be integers
-struct rectangle {
+struct rectangle
+{
     Point topRight;
     Point bottomLeft;
 };
 
-struct node_ele {
+struct node_ele
+{
     Rect mbr;
-    Node* child;
+    Node *child;
 };
 
-struct node {
+struct node
+{
     bool is_leaf;
-    int count;      // no of entries stored
-    Node_ele* elements;
+    int count; // no of entries stored
+    Node_ele *elements;
     // add parent node if required
 };
 
-struct rtree {
-    Node* root;
+struct rtree
+{
+    Node *root;
 };
 
-
 /* DECLARE ALL THE FUNCTION DEFINITIONS HERE */
-Rtree createRTree();
+Rtree *createRTree();
 int CalculateAreaOfrectangle(Rect rec);
-Node* chooseSubTree(Node* n);
-int LeafCheck(Node* n);
-Node* descendTree(Node* n);
-Rect ChooseLeaf(Node* r);
+Rect CalculateRecatangleMBR(Node *n);
+int CalculateOverlapArea(Rect rect1, Rect rect2);
+Node *chooseSubTree(Node *n);
+int LeafCheck(Node *n);
+Node *descendTree(Node *n);
+Node *ChooseLeaf(Rtree *r, Rect r);
 
-void pickSeeds(Node* node, Rect* r1, Rect* r2);
+void pickSeeds(Node *node, Rect *r1, Rect *r2);
 void pickNext();
 
-Rect create_rectangle(int xmin, int ymin, int xmax,int ymax);
+Rect create_rectangle(int xmin, int ymin, int xmax, int ymax);
 bool is_overlap(Rect r, Rect mbr);
 void search(Node *search_node, Rect search_rect);
 
